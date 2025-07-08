57ffeb2eb81ac785ca72a904ce3934e02462ee7f
Al3x-Myku <micu.georgealexandru@gmail.com>
2025-07-08 19:01:08 +0300

    ganache

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -30,9 +30,56 @@ bytecode = compile_sol["contracts"]["SimpleStorgae.sol"]["SimpleStorage"]["evm"]
 abi = compile_sol["contracts"]["SimpleStorgae.sol"]["SimpleStorage"]["abi"]
 
 w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
-chain_id = 5777
+chain_id = 1337
 my_address = "0xCb22C032d64f92ae1aB0EE923EEA1Ad5a2b96d11"
+#nu da push la key in plain txt
 private_key = "0xfa0ba31965d5c70039350bd3f90ec8a9ac153f98abb15e93ff8fff5b1a813aa5"
 
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
 
+nonce = w3.eth.get_transaction_count(my_address)
+
+#build transaction
+# sign transaction
+# send transaction
+transaction = SimpleStorage.constructor().build_transaction(
+    {
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+
+#acum semnezi
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+
+# trimite transaction
+# Look for where you send the raw transaction
+# Change .rawTransaction to .raw_transaction
+txn_hash = w3.eth.send_raw_transaction(signed_txn.raw_transaction)
+
+#Good practice to wait for the transaction to be mined
+txn_receipt = w3.eth.wait_for_transaction_receipt(txn_hash)
+
+print("Deployed!")
+
+#interactiune cu contractul
+simple_storage = w3.eth.contract(address=txn_receipt.contractAddress, abi=abi)
+# print(simple_storage.functions.retrieve().call())
+#Call -> returns a value
+#Transact -> changes a value
+# print(simple_storage.functions.store(15).call()) 15
+# print(simple_storage.functions.retrieve().call()) 0
+
+store_transaction = simple_storage.functions.store(15).build_transaction(
+    {
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": w3.eth.get_transaction_count(my_address),
+    }
+)
+signed_store_txn = w3.eth.account.sign_transaction(store_transaction, private_key=private_key)
+store_txn_hash = w3.eth.send_raw_transaction(signed_store_txn.raw_transaction)
+store_txn_receipt = w3.eth.wait_for_transaction_receipt(store_txn_hash)
+
+print(simple_storage.functions.retrieve().call())  # Should print 15
